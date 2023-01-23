54371132746190443bf533f8f1bc20361320a797
SiggiS <siggis@gmail.com>
2023-01-23 16:57:48 +0100

    First VR experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
+import { FC } from 'react';
+import { Engine, Scene } from 'react-babylonjs';
+import { Vector3 } from '@babylonjs/core/Maths/math.vector';
+
+const App: FC = () => {
+  return (
+    <div style={{ flex: 1, display: 'flex' }}>
+      <Engine
+        antialias
+        adaptToDeviceRatio
+        canvasId="babylon-js"
+        renderOptions={{
+          whenVisibleOnly: true,
+        }}
+      >
+        <Scene>
+          <freeCamera
+            name="camera1"
+            position={new Vector3(0, 5, -10)}
+            setTarget={[Vector3.Zero()]}
+          />
+          <hemisphericLight
+            name="light1"
+            intensity={0.9}
+            direction={new Vector3(0, 1, 0)}
+          />
+          <ground name="ground" width={6} height={6} />
+        </Scene>
+      </Engine>
+    </div>
+  );
+}
+
+/*
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
@@ -7,12 +41,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>mtvs4u</h2>
-        PWA-MVP#01
+        PWA with Babylon.js
         <p><img src={logo} className="App-logo" alt="logo" /></p>
         <p>https://github.com/siegfriedschaefer/bjs-pwa-workshop.git</p>
       </header>
     </div>
   );
 }
+*/
 
 export default App;
